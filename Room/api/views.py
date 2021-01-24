f55f8ad736700924280cfe43ad9edb4e88fec841
zarif007 <zarifhuq786@gmail.com>
2021-01-25 04:22:14 +0600

    Set up Django, React, Webpack, Babel

---
+++ Room/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+
+
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
